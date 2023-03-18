eae0dcac7aa85734e882c5528246bf5fdd708d0c
Yan <jascichy@wp.pl>
2023-03-18 20:58:15 +0100

    Fix problems with email service (it started working)

--- src/main/java/com/team3/central/services/EmailService.java
+++ src/main/java/com/team3/central/services/EmailService.java
@@ -1,17 +1,5 @@
 package com.team3.central.services;
 
-import java.io.IOException;
-import java.util.Map;
-
-import javax.mail.MessagingException;
-
-
-/**
- * Created by Olga on 8/22/2016.
- */
 public interface EmailService {
-    void sendSimpleMessage(String to,
-                           String subject,
-                           String text);
-
+    void sendSimpleMessage(String to, String subject, String text);
 }
--- src/test/java/com/team3/central/EmailServiceTest.java
+++ src/test/java/com/team3/central/EmailServiceTest.java
@@ -2,15 +2,17 @@ package com.team3.central;
 
 import com.team3.central.services.EmailServiceImpl;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class EmailServiceTest {
+    @Autowired
+    EmailServiceImpl emailService;
 
     @Test
     void sendEmailTest() {
-        EmailServiceImpl emailService =new EmailServiceImpl();
-        emailService.sendSimpleMessage();
+        emailService.sendSimpleMessage("io2testmail@gmail.com", "test", "test content");
     }
 
 }
