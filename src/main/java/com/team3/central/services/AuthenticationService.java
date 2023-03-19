5ee06980fe6cf2560bfa3bb888fd686f414b2cdf
Adam Dabkowski <101904659+adamdab@users.noreply.github.com>
2023-03-19 23:46:40 +0100

    Connection do DB

--- src/main/java/com/team3/central/services/AuthenticationService.java
+++ src/main/java/com/team3/central/services/AuthenticationService.java
@@ -1,13 +1,8 @@
 package com.team3.central.services;
 
-import com.team3.central.controllers.AuthenticationRequest;
-import com.team3.central.controllers.AuthenticationResponse;
-import com.team3.central.controllers.RegisterRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -19,26 +14,26 @@ public class AuthenticationService {
   private final JwtService jwtService;
   private final AuthenticationManager authenticationManager;
 
-  public AuthenticationResponse register(RegisterRequest request) {
-    var user = User.builder()
-        .username(request.getEmail())
-        .password(passwordEncoder.encode(request.getPassword()))
-        .build();
-    //repository.save(user);
-    var jwtToken = jwtService.generateToken(user);
-    return AuthenticationResponse.builder().token(jwtToken).build();
-  }
+//  public AuthenticationResponse register(RegisterRequest request) {
+//    var user = User.builder()
+//        .username(request.getEmail())
+//        .password(passwordEncoder.encode(request.getPassword()))
+//        .build();
+//    //repository.save(user);
+//    var jwtToken = jwtService.generateToken(user);
+//    return AuthenticationResponse.builder().token(jwtToken).build();
+//  }
 
-  public AuthenticationResponse authenticate(AuthenticationRequest request) {
-    authenticationManager.authenticate(
-        new UsernamePasswordAuthenticationToken(
-            request.getEmail(),
-            request.getPassword()
-        )
-    );
-    //var user = repository.findByEmail(request.getEmail()).orElseThrow(new Exception());
-    //var jwtToken = jwtService.generateToken(user);
-    //return AuthenticationResponse.builder().token(jwtToken).build();
-    return null;
-  }
+//  public AuthenticationResponse authenticate(AuthenticationRequest request) {
+//    authenticationManager.authenticate(
+//        new UsernamePasswordAuthenticationToken(
+//            request.getEmail(),
+//            request.getPassword()
+//        )
+//    );
+//    //var user = repository.findByEmail(request.getEmail()).orElseThrow(new Exception());
+//    //var jwtToken = jwtService.generateToken(user);
+//    //return AuthenticationResponse.builder().token(jwtToken).build();
+//    return null;
+//  }
 }
--- src/test/java/com/team3/central/services/EmailServiceTest.java
+++ src/test/java/com/team3/central/services/EmailServiceTest.java
@@ -1,18 +1,16 @@
-package com.team3.central;
+package com.team3.central.services;
 
-import com.team3.central.services.EmailServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class EmailServiceTest {
     @Autowired
-    EmailServiceImpl emailService;
+    EmailService emailService;
 
     @Test
     void sendEmailTest() {
-        emailService.sendSimpleMessage("io2testmail@gmail.com", "test", "test content");
+        //emailService.sendSimpleMessage("io2testmail@gmail.com", "test", "test content");
     }
 
 }
