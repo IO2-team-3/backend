c4c0849d6b7847b540fa1c3b501f2d2938a55e53
Yan <jascichy@wp.pl>
2023-03-25 20:52:09 +0100

    Start fixing style issues

--- src/main/java/com/team3/central/configuration/EmailConfiguration.java
+++ src/main/java/com/team3/central/configuration/EmailConfiguration.java
@@ -1,6 +1,7 @@
 package com.team3.central.configuration;
 
 import java.util.Properties;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
