10fa6323142d981fdf7e154744e1f3f5b56ca921
Jan Cichomski <41972182+CJ4n@users.noreply.github.com>
2023-03-28 23:49:29 +0200

    Make code style consistent

---
+++ src/main/java/com/team3/central/security/config/ApplicationConfig.java
@@ -0,0 +1,32 @@
+package com.team3.central.security.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+//  @Bean
+//  public AuthenticationProvider authenticationProvider() {
+//    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+//    authProvider.setUserDetailsService(userDetailsService());
+//    authProvider.setPasswordEncoder(passwordEncoder());
+//    return  authProvider;
+//  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)
+      throws Exception {
+    return configuration.getAuthenticationManager();
+  }
+
+  @Bean
+  public BCryptPasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+}
