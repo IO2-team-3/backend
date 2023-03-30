b86e682f4eb97a7d3bc514dced3484b2a797dd19
AleksyBalazinski <aleksy.j.balazinski@gmail.com>
2023-03-30 21:42:10 +0200

    adding categories

---
+++ src/main/java/com/team3/central/services/CategoriesService.java
@@ -0,0 +1,42 @@
+package com.team3.central.services;
+
+import com.team3.central.mappers.CategoryMapper;
+import com.team3.central.repositories.CategoryRepository;
+import com.team3.central.repositories.entities.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CategoriesService {
+    private final CategoryRepository categoryRepository;
+    private final CategoryMapper categoryMapper;
+
+    @Autowired
+    public CategoriesService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+        this.categoryMapper = new CategoryMapper();
+    }
+
+    public com.team3.central.openapi.model.Category addCategory(String name) {
+        Category category = new Category(name);
+
+        categoryRepository.save(category);
+        return categoryMapper.convertToModel(category);
+    }
+
+    public List<com.team3.central.openapi.model.Category> getAllCategories() {
+        return categoryRepository.findAll()
+                .stream()
+                .map(categoryMapper::convertToModel)
+                .collect(Collectors.toList());
+    }
+
+    public boolean existsByName(String name) {
+        return categoryRepository.findAll()
+                .stream()
+                .anyMatch(c -> c.getName().equals(name));
+    }
+}
---
+++ src/test/java/com/team3/central/services/CategoriesServiceTest.java
@@ -0,0 +1,5 @@
+package com.team3.central.services;
+
+public class CategoriesServiceTest {
+
+}
