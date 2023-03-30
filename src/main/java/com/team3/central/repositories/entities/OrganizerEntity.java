1ce6aa89b1d77d2a9edc866c0698074c482df75b
Yan <jascichy@wp.pl>
2023-03-30 21:58:39 +0200

    Rebase onto master

--- src/main/java/com/team3/central/repositories/entities/OrganizerEntity.java
+++ src/main/java/com/team3/central/repositories/entities/OrganizerEntity.java
@@ -43,6 +43,7 @@ public class OrganizerEntity implements UserDetails {
     this.name = name;
     this.email = email;
     this.password = password;
+    this.isAuthorised=false;
   }
 
 
--- src/main/java/com/team3/central/services/OrganizerService.java
+++ src/main/java/com/team3/central/services/OrganizerService.java
@@ -24,7 +24,7 @@ public class OrganizerService {
   private final JwtService jwtService;
 
   // Generates new confirmation token for given OrganizerEntity and saves it to database
-  private @NotNull ConfirmationToken generateConfirmationToken(OrganizerEntity organizerEntity) {
+  public @NotNull ConfirmationToken generateConfirmationToken(OrganizerEntity organizerEntity) {
     ConfirmationToken confirmationToken = new ConfirmationToken(organizerEntity);
     confirmationTokenService.saveConfirmationToken(confirmationToken);
     return confirmationToken;
@@ -42,9 +42,7 @@ public class OrganizerService {
   // If organizer exists and has been authorized -> do nothing -> code = 400
   public ResponseEntity<OrganizerEntity> signUp(String name, String email,
       String password) {
-    boolean organizerExists = organizerRepository
-        .findByEmail(email)
-        .isPresent();
+    boolean organizerExists = organizerRepository.findByEmail(email).isPresent();
 
     if (organizerExists) {
       var organizer = organizerRepository.findByEmail(email).get();
