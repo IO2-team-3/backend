bec8c4aa4234686d61734f523ec74cdd6394538b
Yan <jascichy@wp.pl>
2023-04-23 13:07:54 +0200

    Fix: adding/deleting reservation will update event.freePlace

--- src/main/java/com/team3/central/repositories/EventRepository.java
+++ src/main/java/com/team3/central/repositories/EventRepository.java
@@ -3,6 +3,8 @@ import com.team3.central.repositories.entities.Category;
 import com.team3.central.repositories.entities.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -18,4 +20,9 @@ public interface EventRepository extends JpaRepository<Event, Long> {
     List<Event> findAll();
 
     List<Event> findByCategories(Category category);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Event e SET e.freePlace = :freePlace WHERE e.id = :id")
+    int updateEventById(Long id, Long freePlace);
 }
