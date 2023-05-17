bc4187eba5a3d7404b2882fc8d6dd1c6a74a672d
adamdab <adamdabkowski01@gmail.com>
2023-05-17 11:07:48 +0200

    added S3 configs + service + example endpoint

--- src/main/java/com/team3/central/controllers/HealthCheck.java
+++ src/main/java/com/team3/central/controllers/HealthCheck.java
@@ -1,6 +1,9 @@
 package com.team3.central.controllers;
 
+import com.amazonaws.HttpMethod;
+import com.team3.central.services.AwsS3Service;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class HealthCheck {
 
+  @Autowired
+  private final AwsS3Service awsS3Service;
   @GetMapping("/ping")
   String getPing() {
     return "pong";
+  }
+
+  @GetMapping("/url")
+  String getUrlToS3() {
+    // we allow for methods : GET, PUT and DELETE
+    // fila path should be like "/some/place/filename.type" eg "/events/1/photo.png"
+    return awsS3Service.generatePreSignedUrl("test.png","io2-central-photos", HttpMethod.DELETE);
   }
 
 }
